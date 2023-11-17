86d345df7d6b784cadcedc46c21984233d07fba3
MarquesGusta <gugamarquues2004@gmail.com>
2023-11-17 16:43:30 -0300

    test(index.js): teste de rede
    teste de rede se há usuários no banco ou não para adicionar adms

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const script = require("./models/ademiro/adminInsert")
 // Colocando variaveis de ambiente
 
 const dotenv = require("dotenv")
@@ -14,24 +15,31 @@ app.use(cors())
 //recebendo arquivos em formato json
 app.use(express.json())
 
-// variavel de conexão com o banco
-const connect = require('../services/connect')
-connect();
-
 // rotas 
 const routes = require('./routes/router');
+// const produto = require("./routes/produto/produto_post")
 
 app.use('/api', routes)
+// app.use("/produto", produto)
 
+app.post("/logar" , (req,res,next) =>{
+    const login = req.body.login
+    const senha = req.body.password
+
+    console.log("Login e senha: " + login + " " + senha)
+
+})
 // ouvindo uma porta
 
 const mongoose = require("mongoose")
  
 mongoose.connect("mongodb://gllmm:GvjzNgjDXFT29o0S7bmq6q9ww6JAAdltwa0aaMhM98Vamxx2JrI3oP3JBtUbyvRWgNlEcfDDVaZgACDbwN9H9w==@gllmm.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@gllmm@")
 .then(() =>{
+
+    script()
     console.log("Conectado ao banco com sucesso.")
-    app.listen( server_port, () =>{
-        console.log("Servidor aberto na porta: " + server_port)
+    app.listen(  3000, "0.0.0.0", () =>{
+        console.log("Servidor aberto na porta: " + "3000")
    
     })
    
---
+++ backend/routes/produto/produto_post.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router =  express.Router
+
+
+// /produto/inserir
+router.post("/inserir" , (req, res,next) =>{
+
+
+
+
+})
+
+
+// module.exports	= router
