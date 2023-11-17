aeb964b087689a636e2b72615f4b780af849ef69
MarquesGusta <gugamarquues2004@gmail.com>
2023-11-17 14:48:27 -0300

    build(.env): add local ambient variables

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Colocando variaveis de ambiente
+
+const dotenv = require("dotenv")
+
+dotenv.config()
+
 //permite que a API receba dados cross Origin
 app.use(cors())
 
@@ -18,6 +24,19 @@ const routes = require('./routes/router');
 app.use('/api', routes)
 
 // ouvindo uma porta
-app.listen(3000, function(){
-    console.log('conectado');
+
+const mongoose = require("mongoose")
+ 
+mongoose.connect("mongodb://gllmm:GvjzNgjDXFT29o0S7bmq6q9ww6JAAdltwa0aaMhM98Vamxx2JrI3oP3JBtUbyvRWgNlEcfDDVaZgACDbwN9H9w==@gllmm.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@gllmm@")
+.then(() =>{
+    console.log("Conectado ao banco com sucesso.")
+    app.listen( server_port, () =>{
+        console.log("Servidor aberto na porta: " + server_port)
+   
+    })
+   
 })
+.catch((erro) =>{
+    console.log("Erro ao conectar ao banco de dados: " + erro)
+})
+ 
