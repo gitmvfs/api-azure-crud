a4be151796be731e78733f455e71acb9e313058e
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2023-11-30 13:39:37 -0300

    Update insertUser.js

--- models/user/insertUser.js
+++ models/user/insertUser.js
@@ -3,6 +3,7 @@ const modelUser = require('./userModel');
 async function saveUser(body){
 
     const newUser  = new modelUser({
+        telefone: body.telefone,
         email: body.email,
         senha: body.senha
     });
@@ -15,6 +16,7 @@ async function queryUsuario(query){
 
     //Consulta o usuário no banco
     const userSelected = await modelUser.findOne({
+        telefone: query.telefone,
         email: query.email,
         senha: query.senha
     });
